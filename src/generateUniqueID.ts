9a81874389f779ab350e9c30e87e8c3069b3df50
utkarsh <srivastavautkarsh8097@gmail.com>
2019-11-18 02:07:08 +0530

    Added docs

--- src/generateUniqueID.ts
+++ src/generateUniqueID.ts
@@ -1,54 +1,84 @@
 const { nextID, getTimestamp } = require('../build/Release/snowflake');
-import getMacID from './getMacID'
+import getMacID from './getMacID';
 
 const SEQUENCE_BITS = 12;
 const CUSTOM_EPOCH = 1546300800000; // 01-01-2019
 const maxSequence = Math.pow(2, SEQUENCE_BITS) - 1;
 
+/**
+ * Constructs a UniqueID object which stores method for generation
+ * of a unique 64 bit time sortable id and a method for retreiving
+ * time of creation for the ids
+ * 
+ * @param {number} [customEpoch = 1546300800000] A 42 bit long custom epoch
+ * in ms, defaults to 1546300800000 (01-01-2019)
+ * 
+ * ```
+ * const uid = new UniqueID();
+ * await uid.init(); // If not initialised then an error will be thrown
+ * const ID = uid.getUniqueID();
+ * const IDCreateAt = uid.getTimestampFromID(ID);
+ * const currentMacID = uid.getMacID();
+ * ```
+ */
 export class UniqueID {
-    private CUSTOM_EPOCH: number;
-    private MACID: string = '';
-    private lastTimestamp = CUSTOM_EPOCH;
-    private sequence = 0;
+    private _CUSTOM_EPOCH: number;
+    private _MACID: string = '';
+    private _lastTimestamp = CUSTOM_EPOCH;
+    private _sequence = 0;
 
     constructor(customEpoch?: number) {
-        this.CUSTOM_EPOCH = customEpoch || CUSTOM_EPOCH;
+        this._CUSTOM_EPOCH = customEpoch || CUSTOM_EPOCH;
     }
 
-    async init() {
-        const value = await getMacID();
-        this.MACID = value.macIDString;
+    /**
+     * Initialise the mac id for internal computations
+     */
+    async init(): Promise<void> {
+        const { macIDString } = await getMacID();
+        this._MACID = macIDString;
     }
 
+    /**
+     * Generates a unique time sortable 64 bit number using native code
+     * @returns {number} The unique id
+     */
     getUniqueID(): number {
-        if (this.MACID) {
+        if (this._MACID) {
             const currentTimestamp = Date.now();
-            const customCurrentTimeStamp = currentTimestamp - this.CUSTOM_EPOCH;
-            const customLastTimestamp = this.lastTimestamp - this.CUSTOM_EPOCH;
+            const customCurrentTimeStamp = currentTimestamp - this._CUSTOM_EPOCH;
+            const customLastTimestamp = this._lastTimestamp - this._CUSTOM_EPOCH;
             if (customCurrentTimeStamp === customLastTimestamp) {
-                this.sequence = (this.sequence + 1) & maxSequence;
-                if (this.sequence === 0) {
+                this._sequence = (this._sequence + 1) & maxSequence;
+                if (this._sequence === 0) {
                     return this.getUniqueID();
                 }
             }
             else {
-                this.sequence = 0;
+                this._sequence = 0;
             }
 
-            this.lastTimestamp = currentTimestamp;
+            this._lastTimestamp = currentTimestamp;
 
-            return nextID(customCurrentTimeStamp, customLastTimestamp, this.sequence, this.MACID);
+            return nextID(customCurrentTimeStamp, customLastTimestamp, this._sequence, this._MACID);
         }
         else {
             throw new Error('Uninitialised class');
         }
     }
 
+    /**
+     * Retrieves the epoch/unix timestamp at which the ID was generated
+     * irrespective of the machine it was generated on, PROVIDED no or same 
+     * custom epoch was used in generation
+     * @param id {number} id generated through getUniqueID method
+     * @returns {number} timestamp of id creations
+     */
     getTimestampFromID(id: number): number {
-        return getTimestamp(id) + this.CUSTOM_EPOCH;
+        return getTimestamp(id) + this._CUSTOM_EPOCH;
     }
 
     getMacID(): string {
-        return this.MACID;
+        return this._MACID;
     }
 }
--- src/getMacID.ts
+++ src/getMacID.ts
@@ -1,11 +1,24 @@
 import { getMac } from 'getmac';
 
 interface MacID {
+    /**
+     * MACID of the machine 
+     */
     macID: string;
+    /**
+     * Special format of MACID without colons
+     * @example
+     * MACID -> "02:aa:43:56:a4:67" -> "02aa4356a467"
+     */
     macIDString: string;
 }
 
-export default async (): Promise<MacID> => {
+/**
+ * Asynchronous function returns mac address of the machine
+ * @returns {Promise<MacID>} A promise to an object containing macID and 
+ * special representation of it
+ */
+const getMacID = async (): Promise<MacID> => {
     return new Promise((resolve, reject) => {
         getMac((err: Error, macID: string) => {
             if (err) reject(err);
@@ -13,3 +26,7 @@ export default async (): Promise<MacID> => {
         })
     })
 }
+
+export { getMacID };
+
+export default getMacID;
