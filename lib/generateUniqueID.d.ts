8facae2b705c5162485e58ab375dcfb3897981a8
utkarsh <srivastavautkarsh8097@gmail.com>
2019-11-18 02:07:32 +0530

    Build

--- lib/generateUniqueID.d.ts
+++ lib/generateUniqueID.d.ts
@@ -1,11 +1,41 @@
+/**
+ * Constructs a UniqueID object which stores method for generation
+ * of a unique 64 bit time sortable id and a method for retreiving
+ * time of creation for the ids
+ *
+ * @param {number} [customEpoch = 1546300800000] A 42 bit long custom epoch
+ * in ms, defaults to 1546300800000 (01-01-2019)
+ *
+ * ```
+ * const uid = new UniqueID();
+ * await uid.init(); // If not initialised then an error will be thrown
+ * const ID = uid.getUniqueID();
+ * const IDCreateAt = uid.getTimestampFromID(ID);
+ * const currentMacID = uid.getMacID();
+ * ```
+ */
 export declare class UniqueID {
-    private CUSTOM_EPOCH;
-    private MACID;
-    private lastTimestamp;
-    private sequence;
+    private _CUSTOM_EPOCH;
+    private _MACID;
+    private _lastTimestamp;
+    private _sequence;
     constructor(customEpoch?: number);
+    /**
+     * Initialise the mac id for internal computations
+     */
     init(): Promise<void>;
+    /**
+     * Generates a unique time sortable 64 bit number using native code
+     * @returns {number} The unique id
+     */
     getUniqueID(): number;
+    /**
+     * Retrieves the epoch/unix timestamp at which the ID was generated
+     * irrespective of the machine it was generated on, PROVIDED no or same
+     * custom epoch was used in generation
+     * @param id {number} id generated through getUniqueID method
+     * @returns {number} timestamp of id creations
+     */
     getTimestampFromID(id: number): number;
     getMacID(): string;
 }
--- lib/getMacID.d.ts
+++ lib/getMacID.d.ts
@@ -1,7 +1,21 @@
 interface MacID {
+    /**
+     * MACID of the machine
+     */
     macID: string;
+    /**
+     * Special format of MACID without colons
+     * @example
+     * MACID -> "02:aa:43:56:a4:67" -> "02aa4356a467"
+     */
     macIDString: string;
 }
-declare const _default: () => Promise<MacID>;
-export default _default;
+/**
+ * Asynchronous function returns mac address of the machine
+ * @returns {Promise<MacID>} A promise to an object containing macID and
+ * special representation of it
+ */
+declare const getMacID: () => Promise<MacID>;
+export { getMacID };
+export default getMacID;
 //# sourceMappingURL=getMacID.d.ts.map
