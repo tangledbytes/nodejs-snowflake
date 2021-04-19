b8aa1f9a44ac4b38d9f0c3607614906db1da66a2
Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>
2021-04-20 03:21:27 +0530

    expose getIDFromTimestamp
    Signed-off-by: Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>

--- src/generateUniqueID.ts
+++ src/generateUniqueID.ts
@@ -1,31 +1,29 @@
-import isFalsy from './isFalsy'
+import isFalsy from "./isFalsy";
 
-const { Snowflake } = require('../build/Release/snowflake');
+const { Snowflake } = require("../build/Release/snowflake");
 
 const CUSTOM_EPOCH = 1546300800000; // 01-01-2019
 const MAX_MACHINE_ID = (1 << 12) - 1;
 
 interface Config {
-    customEpoch?: number;
-    returnNumber?: boolean;
-    machineID?: number;
+  customEpoch?: number;
+  returnNumber?: boolean;
+  machineID?: number;
 }
 
 const initConfig: Config = {
-    customEpoch: CUSTOM_EPOCH,
-    returnNumber: false
-}
-
-
+  customEpoch: CUSTOM_EPOCH,
+  returnNumber: false,
+};
 
 /**
  * Constructs a UniqueID object which stores method for generation
  * of a unique 64 bit time sortable id and a method for retreiving
  * time of creation for the ids
- * 
+ *
  * @param {config} config
  * in ms, defaults to 1546300800000 (01-01-2019)
- * 
+ *
  * ```
  * const uid = new UniqueID();
  * const ID = uid.getUniqueID();
@@ -33,73 +31,109 @@ const initConfig: Config = {
  * ```
  */
 export class UniqueID {
-    private _CUSTOM_EPOCH: number;
-    private _snowflake: any;
-    private _MACHINE_ID: number;
-    private returnNumber = true;
+  private _CUSTOM_EPOCH: number;
+  private _snowflake: any;
+  private _MACHINE_ID: number;
+  private returnNumber = true;
 
-    constructor(config: Config = initConfig) {
-        this._CUSTOM_EPOCH = config.customEpoch || CUSTOM_EPOCH;
-        this.returnNumber = !!config.returnNumber;
+  constructor(config: Config = initConfig) {
+    this._CUSTOM_EPOCH = config.customEpoch || CUSTOM_EPOCH;
+    this.returnNumber = !!config.returnNumber;
+    this._MACHINE_ID = getMachineID(config.machineID);
 
-        // A 12 bit machine id, if not passed in then a random id will be used
-        // Ternary operator was used to make sure "0" isn't considered to be falsy.
-        this._MACHINE_ID = (
-            !isFalsy(config.machineID) ? config.machineID : Math.floor(Math.random() * MAX_MACHINE_ID)
-        ) as number;
+    this._snowflake = new Snowflake(this._CUSTOM_EPOCH, this._MACHINE_ID);
+  }
 
-        // Check if the number is satisfies all the conditions
-        if (!Number.isInteger(this._MACHINE_ID)) throw Error("Machine Id should be a decimal number");
-        if (this._MACHINE_ID > MAX_MACHINE_ID) throw Error("Maximum value of machine id can be 2^12 - 1 (4095)")
+  /**
+   * Generates a unique time sortable 64 bit number using native code
+   * @returns {string | bigint} the unique id
+   */
+  getUniqueID(): string | bigint {
+    const val = this._snowflake.getUniqueID();
+    if (!this.returnNumber) return val.toString();
+    return val;
+  }
 
-        this._snowflake = new Snowflake(this._CUSTOM_EPOCH, this._MACHINE_ID);
-    }
+  /**
+   * Promisified unique id function
+   * @returns {Promise<string | number>} promise to a unique 64 bit long id
+   */
+  async asyncGetUniqueID(): Promise<string | bigint> {
+    return new Promise((resolve) => resolve(this.getUniqueID()));
+  }
 
-    /**
-     * Generates a unique time sortable 64 bit number using native code
-     * @returns {string | bigint} the unique id
-     */
-    getUniqueID(): string | bigint {
-        const val = this._snowflake.getUniqueID()
-        if (!this.returnNumber)
-            return val.toString();
-        return val
-    }
+  /**
+   * Retrieves the epoch/unix timestamp at which the ID was generated
+   * irrespective of the machine it was generated on, PROVIDED no or same
+   * custom epoch was used in generation
+   * @param {number | string} id generated through getUniqueID method
+   * @returns {number} timestamp of id creations
+   */
+  getTimestampFromID(id: bigint | string): number {
+    return this._snowflake.getTimestampFromID(id);
+  }
 
-    /**
-     * Promisified unique id function
-     * @returns {Promise<string | number>} promise to a unique 64 bit long id
-     */
-    async asyncGetUniqueID(): Promise<string | bigint> {
-        return new Promise(resolve => resolve(this.getUniqueID()))
-    }
+  /**
+   * Retrieves the 12 bit machine id where the id was generated,
+   * irrespective of the machine it was generated on.
+   * @param {bigint | string} id unique ID
+   * @returns {number} machine ID
+   */
+  getMachineIDFromID(id: bigint | string): number {
+    return this._snowflake.getNodeIDFromID(id);
+  }
 
-    /**
-     * Retrieves the epoch/unix timestamp at which the ID was generated
-     * irrespective of the machine it was generated on, PROVIDED no or same 
-     * custom epoch was used in generation
-     * @param {number | string} id generated through getUniqueID method
-     * @returns {number} timestamp of id creations
-     */
-    getTimestampFromID(id: bigint | string): number {
-        return this._snowflake.getTimestampFromID(id);
-    }
+  /**
+   * getIDFromTimestamp generates an ID corresponding to the given timestamp
+   *
+   * This ID is generated with sequence number 0
+   *
+   * Ideal for use cases where a database query requires to entries created after some timestamp
+   * @param {number | string} timestamp timestamp corresponding to which an ID is required
+   * @returns {bigint} ID corresponding to the timestamp
+   */
+  getIDFromTimestamp(timestamp: number | string): bigint {
+    return this._snowflake.getIDFromTimestamp(timestamp);
+  }
 
-    /**
-     * Retrieves the 12 bit machine id where the id was generated,
-     * irrespective of the machine it was generated on.
-     * @param id 
-     */
-    getMachineIDFromID(id: bigint | string): number {
-        return this._snowflake.getNodeIDFromID(id);
-    }
+  /**
+   * Machine ID of the current machine. This ID is of 12 bit.
+   * This can be either provided by the user (preferred) or will be assigned
+   * randomly.
+   */
+  get machineID() {
+    return this._MACHINE_ID;
+  }
+}
 
-    /**
-     * Machine ID of the current machine. This ID is of 12 bit.
-     * This can be either provided by the user (preferred) or will be assigned
-     * randomly.
-     */
-    get machineID() {
-        return this._MACHINE_ID;
-    }
+/**
+ * getMachineID takes in a machine id and verifies if it withstand
+ * the following contraints:
+ * 	
+ * 1. Should not be "falsy" but could be 0
+ * 2. Should be of type "number"
+ * 3. Is a whole number
+ * 4. Doesn't exceeds the limit of 4095
+ * 
+ * It will return a random value ε [0, 4095] if parameter is
+ * undefined
+ * @param mid machine id
+ */
+function getMachineID(mid: number | undefined): number {
+  let id = 0;
+
+  if (isFalsy(mid) || typeof mid !== "number")
+    id = Math.floor(Math.random() * MAX_MACHINE_ID);
+  else id = mid;
+
+  verifyMachineID(id);
+
+  return id;
+}
+
+function verifyMachineID(mid: number) {
+  if (!Number.isInteger(mid))
+    throw Error("Machine Id should be a decimal number");
+  if (mid > MAX_MACHINE_ID)
+    throw Error("Maximum value of machine id can be 2^12 - 1 (4095)");
 }
--- src/isFalsy.ts
+++ src/isFalsy.ts
@@ -1,7 +1,9 @@
-export default (value: any) => {
+export default (value: any): boolean => {
     if (value === undefined) return true;
     if (isNaN(value)) return true;
     if (Array.isArray(value) && !value.length) return true;
     if (value === "") return true;
     if (value === null) return true;
+
+    return false
 }
