7b7bb4eff2cd0f354226a36c54a38b610f6c3204
Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>
2021-04-20 03:19:49 +0530

    lib removed from repository - will be generated in the github actions
    Signed-off-by: Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>

--- lib/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { UniqueID } from './generateUniqueID';
-export { UniqueID };
-export default UniqueID;
-//# sourceMappingURL=index.d.ts.map
--- lib/isFalsy.d.ts
+++
@@ -1,3 +0,0 @@
-declare const _default: (value: any) => true | undefined;
-export default _default;
-//# sourceMappingURL=isFalsy.d.ts.map
